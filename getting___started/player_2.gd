5ddfb6bc0f7f4c3191a515891bafea6433c46950
owen <123703288+almodn@users.noreply.github.com>
2025-02-25 15:21:28 -0600

    added player 2

--- getting___started/player_2.gd
+++ getting___started/player_2.gd
@@ -10,12 +10,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump 2") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move left", "move right")
+	var direction := Input.get_axis("move left 2", "move right 2")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
