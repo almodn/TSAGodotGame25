97ca2ada71ff20571baa95e03a2ad73727c46082
The-Iron-Pastry <fenixmontgomery692@Gmail.com>
2025-03-12 16:31:23 -0500

    Added Box Needs work

--- TSA_GAME/exit.gd
+++ TSA_GAME/exit.gd
@@ -1,9 +1,14 @@
 extends Area2D
+
 @export var destination = ""
+@export var KeyHere = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimatedSprite2D.play("open")
+	if KeyHere == true:
+		$AnimatedSprite2D.play("closed")
+	elif KeyHere == true:
+		$AnimatedSprite2D.play("open")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +17,16 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: CharacterBody2D) -> void:
+	if KeyHere == true:
+		if Global.KeyCollected == true:
+			$AnimatedSprite2D.play("open")
+			$Timer.start()
+	elif KeyHere == false:
+		get_tree().change_scene_to_file(destination)
+
+
+
+
+func _on_timer_timeout() -> void:
+	Global.KeyCollected = false
 	get_tree().change_scene_to_file(destination)
