0975d010e57f9cdec1cede0fefb639fdfec49e89
The-Iron-Pastry <fenixmontgomery692@Gmail.com>
2025-03-20 13:58:29 -0500

    Added texture

---
+++ pushable_box.gd
@@ -0,0 +1,23 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == CharacterBody2D:
+		pass
+
+
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
